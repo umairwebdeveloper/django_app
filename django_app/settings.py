88124441eec81b42cf4f60bef0d9419f22c52bed
umairwebdeveloper <umairashraf5252@gmail.com>
2024-07-04 14:43:33 +0500

    fixed

--- django_app/settings.py
+++ django_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--8nnpns=z7d86)exg!t#a9psbdt-pkj*ht%bfz2i1znqq1sh#0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
