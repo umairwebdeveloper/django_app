f8c4c58c8770220037d8b55018dad9ac0c3c8390
umairwebdeveloper <umairashraf5252@gmail.com>
2024-07-04 16:17:02 +0500

    fixed

--- django_app/settings.py
+++ django_app/settings.py
@@ -125,11 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-
 # allow all subdomains of allowed host
 CORS_ALLOW_ALL_ORIGINS = True
 
