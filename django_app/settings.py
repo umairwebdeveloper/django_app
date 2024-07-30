8bc9590af813692e9256b5e3ac2ea756e45da98c
umairwebdeveloper <umairashraf5252@gmail.com>
2024-07-31 00:03:59 +0500

    fixed

--- django_app/settings.py
+++ django_app/settings.py
@@ -13,11 +13,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from distutils.util import strtobool
 from pathlib import Path
 import os
+import dotenv
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+dotenv.read_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
