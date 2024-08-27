29f49aa5a2120df0f98eb1067914508641a57c47
umairwebdeveloper <umairashraf5252@gmail.com>
2024-08-27 22:32:56 +0500

    fixed

--- django_app/urls.py
+++ django_app/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include 
 from django.conf import settings
 from django.conf.urls.static import static
+from shoefitr.views import iframe_page
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('shoefitr.urls')),
+    path("scan-button/", iframe_page, name="scan_button_main"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
